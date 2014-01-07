838937eaa2d173a227918252ff7bff5450cceb33
bdoughertyOU <bdougherty@omniupdate.com>
2014-01-07 00:59:13 -0800

    starting instagram

--- social.php
+++ social.php
@@ -1,5 +1,6 @@
 <?php
 include 'facebooklibs/auth.php';
+include 'instagramlibs/instagram_auth.php';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -40,8 +41,21 @@ include 'facebooklibs/auth.php';
     <?php endif ?>
     <!--End facebook stuff -->
 
+    <!--Instagram stuff -->
+    <?#php if (isset($_SESSION['InstagramAccessToken']) && !empty($_SESSION['InstagramAccessToken'])):?>
+      <h3>You are using Instagram with Mash'D</h3>
+
+    <?#php else: ?>
+      <div>
+        Login using OAuth 2.0 handled by the PHP SDK:
+       <a href="<?php #echo $instagram_loginUrl; ?>">Instagram Login</a>
+        <strong><em>You are not Connected.</em></strong>
+      </div>
+    <?#php endif ?>
+
+    <!--Instagram stuff  -->
+
     <p><a href="#">Twitter Login</a></p>
-    <p><a href="#">Instagram Login</a></p>
     <p><a href="#">Vine Login</a></p>
     <p><a href="#">Reddit Login</a></p>
 
